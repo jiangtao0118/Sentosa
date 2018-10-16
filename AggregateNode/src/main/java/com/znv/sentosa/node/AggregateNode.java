acf3a98dc7d4ca40fc7c771c8d35c07ae3c7e0c8
jiangtao0118 <1753230173@qq.com>
2018-10-17 00:06:38 +0800

    update AggregateNode

--- AggregateNode/src/main/java/com/znv/sentosa/node/AggregateNode.java
+++ AggregateNode/src/main/java/com/znv/sentosa/node/AggregateNode.java
@@ -3,5 +3,7 @@ package com.znv.sentosa.node;
 public class AggregateNode {
     public static void main(String[] args){
         System.out.println("AggregateNode");
+
+        System.out.println("Test Aggregate");
     }
 }
